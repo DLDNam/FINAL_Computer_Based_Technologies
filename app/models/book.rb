636ee70991bf481919fe4bfb844f83e36bd9834d
Duc <dinhleducnam2003@gmail.com>
2023-11-07 17:20:05 +0700

    Update search books

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,3 +4,17 @@ class Book < ApplicationRecord
   validates_presence_of :title, :author, :publisher
   validates_uniqueness_of :title
 end
+class Book < ApplicationRecord
+  belongs_to :subject
+  validates_presence_of :title, :author, :publisher
+ validates_uniqueness_of :title
+ validates_numericality_of :year
+ def self.search(term)
+  if term
+    where('title LIKE ?', "%#{term}%")
+  else
+    all
+  end
+  end
+  
+end
